6ee905f9e0f4186266d558981d47c6453305d579
MiguelAngelR0 <miguelangelrodriguezescorcia@gmail.com>
2025-01-20 12:26:51 +0100

    1.1.2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'cocktail/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Aquí defines los valores posibles para el parámetro `id`
+      const cocktailIds = ['1', '2']; // Ejemplo: IDs de cócteles
+      return cocktailIds.map(id => ({ id }));
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
